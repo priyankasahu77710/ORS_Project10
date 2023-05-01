8dd779681dd41dd47fb1a9052503a504c33bef7f
priyankasahu77710 <131658624+priyankasahu77710@users.noreply.github.com>
2023-05-01 15:58:29 +0530

    commit

---
+++ projectORS/src/main/java/com/rays/common/message/MessageDAOInt.java
@@ -0,0 +1,11 @@
+package com.rays.common.message;
+
+import com.rays.common.BaseDAOInt;
+
+/**
+ * Role DAO interface.
+ * @author Priyanka sahu
+ */
+public interface MessageDAOInt extends BaseDAOInt<MessageDTO> {
+
+}
